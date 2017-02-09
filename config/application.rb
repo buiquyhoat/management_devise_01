92c2d15a33b5650413f7f0566a0cec8333795190
dattx1 <tran.xuan.dat@framgia.com>
2017-02-09 09:16:24 +0700

    update notification mail

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ module ManagementDevise01
   class Application < Rails::Application
     config.assets.precompile << "audios/*"
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.action_mailer.default_url_options = {host: Settings.mailer_host}
   end
 end
